ff07ee0e8711bbb0502e2bde29052c2e7dbfc34a
Justin <justinyeh13@gmail.com>
2025-02-11 21:00:24 -0500

    Initial Commit

---
+++ Lab1_Justin_Yeh/Lab1_Justin_YehApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Justin_YehApp.swift
+//  Lab1_Justin_Yeh
+//
+//  Created by Justin Yeh on 2025-02-11.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Justin_YehApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
